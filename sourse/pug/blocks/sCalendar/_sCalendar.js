3379d43da8e4e913bbc3201b06147d9724af1be5
Pavel <wol1414@gmail.com>
2020-08-01 01:52:47 +0300

    start

---
+++ sourse/pug/blocks/sCalendar/_sCalendar.js
@@ -0,0 +1,13 @@
+let sCalendarVue = new Vue({
+		el: '#sCalendar',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
---
+++ sourse/pug/blocks/sProfile/_sProfile.js
@@ -0,0 +1,13 @@
+let sProfileVue = new Vue({
+		el: '#sProfile',
+		data: {
+			imgSRc: 'img/',
+		},
+		methods: { 
+		},
+		 created: function () { 
+		},
+		computed: {
+		
+		},
+	})
